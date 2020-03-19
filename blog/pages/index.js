44f2d5fb16f645264e4dd5ddf258422d8484b387
JonesXie <anhuixieqijun@163.com>
2020-03-19 21:53:58 +0800

    project init

---
+++ blog/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+import { Button } from 'antd'
+// import "../reset.css"
+const Home = () => (
+  <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div><Button>我是按钮</Button></div>
+  </>
+)
+
+export default Home
