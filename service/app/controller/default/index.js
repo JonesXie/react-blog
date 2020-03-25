51e692fb8ac79915bcd450e04fad7351aa0d56a2
JonesXie <anhuixieqijun@163.com>
2020-03-25 23:32:19 +0800

    add service

---
+++ service/app/controller/default/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const Controller = require("egg").Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const sql =
+      "SELECT article.id as id," +
+      "article.title as title," +
+      "article.introduce as introduce," +
+      "article.addTime as addTime," +
+      "article.view_count as view_count," +
+      ".type.typeName as typeName " +
+      "FROM article LEFT JOIN type ON article.type_id = type.Id";
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = { data: result };
+  }
+}
+
+module.exports = HomeController;
